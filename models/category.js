5cdf7038527ef0233889eb0bfebfe63c16ada4b6
Moizasif <kmoizasif019@gmail.com>
2021-04-11 21:26:36 +0500

    Outputting a grid of Categories

---
+++ models/category.js
@@ -0,0 +1,10 @@
+class Category {
+    //store data with properties with the help of this keyword
+    constructor(id, title, color) {
+         this.id = id;
+         this.title = title;
+         this.color = color;
+    }
+}
+
+export default Category;
