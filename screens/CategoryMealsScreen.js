c8ca2ce48e7b2f4e889208319cc9298e87976317
Moizasif <kmoizasif019@gmail.com>
2021-04-15 15:38:28 +0500

    Rendering Meal List and Passing data to the Meal Detail Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { View, Text, FlatList, StyleSheet} from 'react-native';
 import {CATEGORIES, MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
 
     const renderMealItem = (itemData) => {
 
         return (
-         <View><Text>{itemData.item.title}</Text></View>
+         <MealItem 
+         title={itemData.item.title} 
+         image={itemData.item.imageUrl}
+         duration={itemData.item.duration}
+         complexity={itemData.item.complexity}
+         affordability={itemData.item.affordability}
+         onSelectMeal={() => {
+             props.navigation.navigate({
+                 routeName: 'MealDetail',
+                 params: {
+                     mealId: itemData.item.id
+                 } 
+             })
+         }}/>
         );
     }
 
@@ -25,7 +39,8 @@ const CategoryMealScreen = (props) => {
              <FlatList 
              keyExtractor = {(item,index) => item.id}
              data={displayedMeals}
-             renderItem={renderMealItem}/>
+             renderItem={renderMealItem}
+             style={{width:'100%'}}/>
         </View>
     );
 };
